f319478b3e136c508eee5c07e91456954b929724
ahmomoz <x5426854268@gmail.com>
2024-09-22 15:47:20 +0800

    Updates

--- assets/index-a5873596.js
+++ assets/index-a5873596.js
@@ -1,4 +1,4 @@
-import"./bootstrap.min-222b2171.js";function c(r){let s="";[{courseName:"Python 基礎入門",img:"course-1.png",teacher:"Anna Wu",star:"4.0"},{courseName:"Web 開發實戰",img:"course-2.png",teacher:"Peter Fan",star:"4.0"},{courseName:"數據分析技巧",img:"course-3.png",teacher:"Neko Meow",star:"4.0"},{courseName:"移動應用開發",img:"course-4.png",teacher:"momomo",star:"4.0"},{courseName:"機器學習導論",img:"course-5.png",teacher:"Tina Chen",star:"4.0"},{courseName:"網絡安全基礎",img:"course-6.png",teacher:"Anna Wu",star:"4.0"},{courseName:"雲計算應用",img:"course-7.png",teacher:"momomo",star:"4.0"},{courseName:"UI/UX 設計原則",img:"course-8.png",teacher:"Tina Chen",star:"4.0"},{courseName:"區塊鏈技術探索",img:"course-9.png",teacher:"Anna Wu",star:"4.0"}].forEach(e=>{s+=`
+import"./bootstrap.min-61f9de2f.js";function o(r,c){let s="";c.forEach(e=>{s+=`
       <div class="col-md-6 col-xl-4">
         <a href="#" class="course-card card-column">
           <div class="card gradient-border p-lg-6 p-4 h-100">
@@ -23,4 +23,4 @@ import"./bootstrap.min-222b2171.js";function c(r){let s="";[{courseName:"Python 
           </div>
         </a>
       </div>
-    `,r.innerHTML=s})}const a=document.querySelector(".course-card-wrap");a?c(a):console.error("course-card-wrap element not found");
+    `,r.innerHTML=s})}const a=document.querySelector(".course-card-wrap"),t=[{courseName:"Python 基礎入門",img:"course-1.png",teacher:"Anna Wu",star:"4.0"},{courseName:"Web 開發實戰",img:"course-2.png",teacher:"Peter Fan",star:"4.0"},{courseName:"數據分析技巧",img:"course-3.png",teacher:"Neko Meow",star:"4.0"},{courseName:"移動應用開發",img:"course-4.png",teacher:"momomo",star:"4.0"},{courseName:"機器學習導論",img:"course-5.png",teacher:"Tina Chen",star:"4.0"},{courseName:"網絡安全基礎",img:"course-6.png",teacher:"Anna Wu",star:"4.0"},{courseName:"雲計算應用",img:"course-7.png",teacher:"momomo",star:"4.0"},{courseName:"UI/UX 設計原則",img:"course-8.png",teacher:"Tina Chen",star:"4.0"},{courseName:"區塊鏈技術探索",img:"course-9.png",teacher:"Anna Wu",star:"4.0"}];a?o(a,t):console.error("course-card-wrap element not found");
