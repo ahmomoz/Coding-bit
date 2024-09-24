c97c795dc9af1afd7e8fcffecc8ac262f5e17b6b
ahmomoz <x5426854268@gmail.com>
2024-09-24 21:24:25 +0800

    Updates

--- assets/index-9f5db742.js
+++ assets/index-9f5db742.js
@@ -1,4 +1,4 @@
-import"./bootstrap.min-ea97e13c.js";function o(r,c){let s="";c.forEach(e=>{s+=`
+import"./bootstrap.min-8ade7b6f.js";function o(r,c){let s="";c.forEach(e=>{s+=`
       <div class="col-md-6 col-xl-4">
         <a href="#" class="course-card card-column">
           <div class="card gradient-border p-lg-6 p-4 h-100">
