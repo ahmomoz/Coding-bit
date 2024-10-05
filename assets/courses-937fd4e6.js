b91e45bbeda4317a36aa862d16fdf469c1de0408
ahmomoz <x5426854268@gmail.com>
2024-10-06 00:47:44 +0800

    Updates

---
+++ assets/courses-937fd4e6.js
@@ -0,0 +1,82 @@
+function o(a,t){let e="";t.forEach(s=>{e+=`
+      <div class="col-md-6 col-xl-4">
+        <a href="#" class="course-card card-column">
+          <div class="card gradient-border img-hover-enlarge p-lg-6 p-4 h-100">
+            <div class="overflow-hidden img-wrapper rounded position-relative">
+              <img
+                src="https://raw.githubusercontent.com/ahmomoz/Coding-bit/refs/heads/main/assets/images/course/${s.img}"
+                class="card-img-top cover-img rounded"
+                alt="course-image">
+            </div>
+            <span class="course-category-tag opacity-75 position-absolute">${s.courseCategory}</span>
+            <div class="card-body p-0 mt-3 mt-lg-4 f-column-between">
+              <div>
+                <h3 class="card-title fs-6 fs-lg-5">${s.courseName}</h3>
+                <p class="card-text fs-7 fs-lg-6 mt-1 mt-lg-2">${s.teacher}</p>
+              </div>
+              <div class="f-between-center">
+                <div class="f-align-center mt-1 mt-lg-2">
+                  <div class="f-center">
+                    <span class="material-symbols-outlined fs-5 me-1">
+                      schedule
+                    </span>
+                    <p class="fs-7 fs-lg-6">
+                      ${s.totalDuration}
+                    </p>
+                  </div>
+                  <div class="f-center ms-2">
+                    <span class="material-symbols-outlined fs-5 me-1">
+                      group
+                    </span>
+                    <p class="fs-7 fs-lg-6">
+                      ${s.purchaseCount}
+                    </p>
+                  </div>
+                </div>
+                <p class="f-center text-brand-03 fs-7 fs-lg-6">
+                  <span class="material-symbols-outlined icon-fill text-brand-01 fs-6 fs-lg-5 me-1">
+                    kid_star
+                  </span>
+                  ${s.star}
+                </p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    `,a.innerHTML=e})}function d(a,t){let e="";t.forEach(s=>{const{chapter:l,title:c,duration:i,viewCount:n}=s,r=`
+    <li class="py-4 d-flex justify-content-between chapter-item">
+      <span class="material-symbols-outlined player-icon me-2">play_circle</span>
+      <div class="flex-grow-1">
+        <div class="d-flex align-items-center justify-content-between mb-1">
+          <span class="fs-7 chapter-contents">${l}</span>
+          <time class="video-duration fs-7 rounded-1 px-2 py-1">${i}</time>
+        </div>
+        <h6 class="chapter-item-title mb-2">${c}</h6>
+        <div class="d-flex">
+          <span class="material-symbols-outlined fs-6 eyes-icon">visibility</span>
+          <data class="chapter-view-count fs-7" value="${n}">${s["view-count"]}</data>
+        </div>
+      </div>
+    </li>`;e+=r}),a.innerHTML=e}function p(a,t){let e="";t.forEach(s=>{const{imageUrl:l,title:c,viewCount:i}=s,n=`
+      <li class="d-flex py-3 py-4 position-relative">
+        <img class="rounded-2 me-4 other-video-image" src="${l}" alt="影片縮圖">
+        <div class="f-column-between py-2">
+          <h6 class="fs-6 other-video-title">${c}</h6>
+          <div class="f-align-center me-6">
+            <span class="view-count me-1 material-symbols-outlined eyes-icon">visibility</span>
+            <data value="${i}" class="data-view-count fs-7">${i}</data>
+          </div>
+        </div>
+      </li>`;e+=n}),a.innerHTML=e}function m(a,t){let e="";t.forEach(s=>{const{imageUrl:l,title:c,viewCount:i,author:n}=s,r=`
+      <li class="d-flex py-3 py-4 position-relative">
+        <img class="rounded-2 me-4 related-video-image" src="${l}" alt="影片縮圖">
+        <div class="f-between-center py-2">
+          <h6 class="fs-6 related-video-title">${c}</h6>
+          <div class="f-align-center me-6">
+            <span>${n}</span>
+            <span class="view-count me-1 material-symbols-outlined eyes-icon">visibility</span>
+            <data value="${i}" class="data-view-count fs-7">${i}</data>
+          </div>
+        </div>
+      </li>`;e+=r}),a.innerHTML=e}export{o as a,d as c,p as o,m as r};
